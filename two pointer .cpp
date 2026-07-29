1caff8db3aac442652938e6141f967a100b0ba73
Sankar Narayanan <srnvsna007@gmail.com>
2026-07-29 20:16:25 +0530

    Implement maxProfit function using two pointers

---
+++ two pointer .cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int minprice=INT_MAX;
+        int maxprice=0;
+
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices[i]<minprice)
+            {
+                minprice = prices[i];
+            }
+            if(prices[i]-minprice>maxprice)
+            {
+                maxprice=prices[i]-minprice;
+            }
+        }
+        return maxprice;
+    }
+};
