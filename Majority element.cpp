03e7b1e384e89c59004675ba0ea0dd090bf5f547
Sankar Narayanan <srnvsna007@gmail.com>
2026-08-03 20:21:02 +0530

    Refactor majorityElement loop and comments
    Refactor loop for clarity and update comments.

--- Majority element.cpp
+++ Majority element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int a =0;//count 
         int b =0;//count of number
-        for(int i =0;i<nums.size();i++)
+        for(int i =0;i<nums.size();i++)//update
         {
             if(a==0)
             {
