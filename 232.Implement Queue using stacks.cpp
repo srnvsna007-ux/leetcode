b209a1fa5da8e11dc307c1c4ae12854f7a54896e
Sankar Narayanan <srnvsna007@gmail.com>
2026-08-06 19:39:19 +0530

    Implement queue using two stacks

---
+++ 232.Implement Queue using stacks.cpp
@@ -0,0 +1,34 @@
+class MyQueue {
+private:
+    stack<int> input;
+    stack<int> output;
+
+public:
+    MyQueue() {
+    }
+
+    void push(int x) {
+        input.push(x);
+    }
+
+    int pop() {
+        peek();
+        int front = output.top();
+        output.pop();
+        return front;
+    }
+
+    int peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
